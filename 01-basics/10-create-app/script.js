0bfbd1108adf103f57f5f235e5ae7f7bcf51c935
Magay Oleg <server90@mail.ru>
2025-03-08 15:34:27 +0300

    feature/10-create-app: Решение задачи 'createApp'

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,27 @@
-import { defineComponent, createApp } from 'vue'
+import { defineComponent, createApp } from 'vue/dist/vue.esm-browser.js'
+
+const App = defineComponent({
+  name: 'App',
+  setup() {
+    function currentDate() {
+      return new Date().toLocaleString(navigator.language, {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      })
+    }
+
+    return {
+      currentDate,
+    }
+  },
+
+  template: `
+    <div>
+      Сегодня {{ currentDate() }}
+    </div>
+  `,
+})
+
+const app = createApp(App)
+app.mount('#app')
