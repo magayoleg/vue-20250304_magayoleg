9f5daf3358942ee641b47525e3b9ae0752dd2675
Magay Oleg <server90@mail.ru>
2025-03-08 23:54:52 +0300

    feature/10-counter: Решение задачи

--- 02-basics-2/10-counter/CounterApp.js
+++ 02-basics-2/10-counter/CounterApp.js
@@ -1,9 +1,25 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 export default defineComponent({
   name: 'CounterApp',
 
-  setup() {},
+  setup() {
+    const count = ref(0)
+
+    function decrement() {
+      count.value--
+    }
+
+    function increment() {
+      count.value++
+    }
+
+    return {
+      decrement,
+      increment,
+      count
+    }
+  },
 
   template: `
     <div class="counter">
@@ -11,15 +27,18 @@ export default defineComponent({
         class="button button--secondary"
         type="button"
         aria-label="Decrement"
-        disabled
+        :disabled="count <= 0"
+        @click="decrement"
       >➖</button>
 
-      <span class="count" data-testid="count">0</span>
+      <span class="count" data-testid="count">{{count}}</span>
 
       <button
         class="button button--secondary"
         type="button"
         aria-label="Increment"
+        :disabled="count >= 5"
+        @click="increment"
       >➕</button>
     </div>
   `,
