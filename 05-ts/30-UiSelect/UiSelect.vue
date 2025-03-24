6d8fe7f3bd5f1ab52c58116fdb2a42522b1055b1
javascriptru <67475495+jsru-1@users.noreply.github.com>
2025-03-24 04:07:38 +0300

    Added solution for task: UiSelect

--- 05-ts/30-UiSelect/UiSelect.vue
+++ 05-ts/30-UiSelect/UiSelect.vue
@@ -1,9 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts" generic="T extends string">
+defineProps<{
+  options: {
+    value: T
+    text: string
+  }[]
+}>()
+
+const model = defineModel<T>()
+</script>
 
 <template>
-  <select class="select">
-    <option value="value-1">One</option>
-    <option value="value-2">Two</option>
+  <select v-model="model" class="select">
+    <option v-for="{ value, text } in options" :key="value" :value>
+      {{ text }}
+    </option>
   </select>
 </template>
 
